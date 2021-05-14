e706ddf458781f7af7650856b951bc92ba79670c
Ken Blizzard-Caron <ken.blizzardcaron@gmail.com>
2021-05-14 15:40:16 -0500

    shorten homepage section a little

--- src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -39,7 +39,7 @@ const Homepage = ({ posts }: PostsProps) => {
       <h1 sx={visuallyHidden}>{siteTitle}</h1>
       <section
         sx={{
-          mb: [5, 6, 7],
+          mb: [4, 5, 6],
           p: { fontSize: [1, 2, 3], mt: 2 },
           variant: `section_hero`,
         }}
