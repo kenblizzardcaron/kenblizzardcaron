6b7a776085a07464dad152d70711df93b83c6236
Ken Blizzard-Caron <ken.blizzardcaron@gmail.com>
2021-05-23 13:43:48 -0500

    intro to TS post

--- src/@lekoarts/gatsby-theme-minimal-blog/components/code.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/code.tsx
@@ -1,23 +1,24 @@
 /* eslint react/destructuring-assignment: 0 */
-import React from "react"
-import Highlight, { defaultProps } from "prism-react-renderer"
-import loadable from "@loadable/component"
-import theme from "prism-react-renderer/themes/nightOwl"
+import React from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import loadable from "@loadable/component";
+import theme from "prism-react-renderer/themes/nightOwl";
+import ts, { transpile } from "typescript";
 
-import Copy from "./copy"
-import useMinimalBlogConfig from "../hooks/use-minimal-blog-config"
-import { Language } from "../types"
+import Copy from "./copy";
+import useMinimalBlogConfig from "../hooks/use-minimal-blog-config";
+import { Language } from "../types";
 
 type CodeProps = {
-  codeString: string
-  language: Language
-  noLineNumbers?: boolean
-  metastring?: string
-  [key: string]: any
-}
+  codeString: string;
+  language: Language;
+  noLineNumbers?: boolean;
+  metastring?: string;
+  [key: string]: any;
+};
 
 function getParams(className = ``) {
-  const [lang = ``, params = ``] = className.split(`:`)
+  const [lang = ``, params = ``] = className.split(`:`);
 
   return [
     // @ts-ignore
@@ -25,35 +26,35 @@ function getParams(className = ``) {
   ].concat(
     // @ts-ignore
     params.split(`&`).reduce((merged, param) => {
-      const [key, value] = param.split(`=`)
+      const [key, value] = param.split(`=`);
       // @ts-ignore
-      merged[key] = value
-      return merged
+      merged[key] = value;
+      return merged;
     }, {})
-  )
+  );
 }
 
-const RE = /{([\d,-]+)}/
+const RE = /{([\d,-]+)}/;
 
 const calculateLinesToHighlight = (meta: string) => {
   if (!RE.test(meta)) {
-    return () => false
+    return () => false;
   }
   const lineNumbers = RE.exec(meta)![1]
     .split(`,`)
-    .map((v) => v.split(`-`).map((x) => parseInt(x, 10)))
+    .map((v) => v.split(`-`).map((x) => parseInt(x, 10)));
   return (index: number) => {
-    const lineNumber = index + 1
+    const lineNumber = index + 1;
     const inRange = lineNumbers.some(([start, end]) =>
       end ? lineNumber >= start && lineNumber <= end : lineNumber === start
-    )
-    return inRange
-  }
-}
+    );
+    return inRange;
+  };
+};
 
 const LazyLiveProvider = loadable(async () => {
-  const Module = await import(`react-live`)
-  const { LiveProvider, LiveEditor, LiveError, LivePreview } = Module
+  const Module = await import(`react-live`);
+  const { LiveProvider, LiveEditor, LiveError, LivePreview } = Module;
   return (props: any) => (
     <LiveProvider {...props}>
       {props.showCopyButton && <Copy content={props.code} />}
@@ -61,8 +62,8 @@ const LazyLiveProvider = loadable(async () => {
       <LiveError />
       <LivePreview data-name="live-preview" />
     </LiveProvider>
-  )
-})
+  );
+});
 
 const Code = ({
   codeString,
@@ -71,22 +72,40 @@ const Code = ({
   metastring = ``,
   ...props
 }: CodeProps) => {
-  const { showLineNumbers, showCopyButton } = useMinimalBlogConfig()
+  const { showLineNumbers, showCopyButton } = useMinimalBlogConfig();
 
-  const [language, { title = `` }] = getParams(blockClassName)
-  const shouldHighlightLine = calculateLinesToHighlight(metastring)
+  const [language, { title = `` }] = getParams(blockClassName);
+  const shouldHighlightLine = calculateLinesToHighlight(metastring);
 
-  const hasLineNumbers = !noLineNumbers && language !== `noLineNumbers` && showLineNumbers
+  const hasLineNumbers =
+    !noLineNumbers && language !== `noLineNumbers` && showLineNumbers;
+
+  const transformCode = (snippet: string, target: ts.ScriptTarget) =>
+    transpile(snippet, {
+      jsx: ts.JsxEmit.React,
+      target,
+    });
 
   if (props[`react-live`]) {
     return (
       <div className="react-live-wrapper">
-        <LazyLiveProvider code={codeString} noInline theme={theme} showCopyButton={showCopyButton} />
+        <LazyLiveProvider
+          code={codeString}
+          noInline
+          showCopyButton={showCopyButton}
+          theme={theme}
+          transformCode={(code) => transformCode(code, ts.ScriptTarget.ES2015)}
+        />
       </div>
-    )
+    );
   }
   return (
-    <Highlight {...defaultProps} code={codeString} language={language} theme={theme}>
+    <Highlight
+      {...defaultProps}
+      code={codeString}
+      language={language}
+      theme={theme}
+    >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <React.Fragment>
           {title && (
@@ -95,24 +114,30 @@ const Code = ({
             </div>
           )}
           <div className="gatsby-highlight" data-language={language}>
-            <pre className={className} style={style} data-linenumber={hasLineNumbers}>
+            <pre
+              className={className}
+              style={style}
+              data-linenumber={hasLineNumbers}
+            >
               {showCopyButton && <Copy content={codeString} fileName={title} />}
               <code className={`language-${language}`}>
                 {tokens.map((line, i) => {
-                  const lineProps = getLineProps({ line, key: i })
+                  const lineProps = getLineProps({ line, key: i });
 
                   if (shouldHighlightLine(i)) {
-                    lineProps.className = `${lineProps.className} highlight-line`
+                    lineProps.className = `${lineProps.className} highlight-line`;
                   }
 
                   return (
                     <div {...lineProps}>
-                      {hasLineNumbers && <span className="line-number-style">{i + 1}</span>}
+                      {hasLineNumbers && (
+                        <span className="line-number-style">{i + 1}</span>
+                      )}
                       {line.map((token, key) => (
                         <span {...getTokenProps({ token, key })} />
                       ))}
                     </div>
-                  )
+                  );
                 })}
               </code>
             </pre>
@@ -120,7 +145,7 @@ const Code = ({
         </React.Fragment>
       )}
     </Highlight>
-  )
-}
+  );
+};
 
-export default Code
+export default Code;
