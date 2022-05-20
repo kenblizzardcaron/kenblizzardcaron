8c3fa6c5754c3a3fba7b80c31a6a229dcd1b6925
Ken Blizzard-Caron <ken.blizzardcaron@gmail.com>
2022-05-20 14:12:03 -0500

    Refactor to be a bit more DRY

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+const DEGREES = '(°F)';
+export const ibtsTemp = `IBTS temp ${DEGREES}`;
+export const IBTS_ROR = 'IBTS-ROR (°F / min)';
+export const IBTS = 'Infrared Bean Temperature Sensor';
+export const ror = `Rate of rise: (°F / min)`;
+export const ROR_LINE_COLOR = '#FF0000';
+export const TEMP_LINE_COLOR = '#8884d8';
